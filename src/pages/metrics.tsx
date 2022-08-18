da25401207ea9ef56e3441de1f0dca44442b4b2b
Warlike Richard <warlikerichard@hotmail.com>
2022-08-18 00:45:15 -0300

    can access page

--- src/pages/metrics.tsx
+++ src/pages/metrics.tsx
@@ -1,25 +1,13 @@
-import { useContext, useEffect } from "react"
-import { AuthContext } from "../../contexts/AuthContext"
 import { withSSRAuth } from "../../utils/withSSRAuth";
 import { Can } from "../components/Can";
 import { setupAPIClient } from "../services/api";
 import { api } from './../services/apiClient';
 
-export default function Dashboard(){
-    const { user } = useContext(AuthContext)
-
-    useEffect(() => {
-        api.get('/me')
-        .then(response => console.log(response))
-        .catch(err => {
-            console.log(err)
-            console.log('dashboard')
-        })
-    }, [])
+export default function Metrics(){
 
     return(
         <>
-            <h1>Dashboard: {user?.email}</h1>
+            <h1>Metrics</h1>
             
             <Can permissions={['metrics.list']}>
                 <div>Métricas</div>
@@ -30,8 +18,12 @@ export default function Dashboard(){
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
     const apiClient  = setupAPIClient(ctx);
+    const response = await apiClient.get('/me')
 
     return {
         props:{}
     }
+}, {
+    permissions: ['metrics.list'],
+    roles: ['administrator']
 })
